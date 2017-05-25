16fc62d25fa3327eb606376b25213bfa0b112d8b
Jose Luis Auza <Mandrake@macbook-pro-de-jose.local>
2017-05-24 20:59:16 -0400

    Initial Commit

---
+++ secondtry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  secondtry
+//
+//  Created by Jose Luis Auza on 24/5/17.
+//  Copyright © 2017 Jose Luis Auza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
